3651db93fb42eb1b34e53258eeda04ccae8e66a1
TusharChakravarti <tusharch04092005@gmail.com>
2025-12-21 16:02:44 +0000

    data_types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all js code as newer version
+// alert(3+3) // we are using node js not browser
+
+console.log(3 + 3)
+console.log("Hitesh")
+
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false
+
+// number  => 2^53
+// bigint
+// string => " "
+// boolean => true/false
+// null => stand alone value
+// undefined => value not assigned
+// symbol =>unique
+
+//object
+
+console.log(typeof "hitesh");
+
+
+console.log(typeof null);// object
+
+console.log(typeof undefined); //undefined
